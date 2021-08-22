bcb4e5804daadeb1cbab58234445af41ea7d2456
Vladislav Paromov <parro@yandex.ru>
2021-08-23 00:45:00 +0300

    feat: index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let str = '' + Math.abs(n);
+    let result = '';
+    for ( let i = str.length-1; i >= 0; i = i - 1) {
+        result =result + str[i];
+    }
+    return result;  
 }
